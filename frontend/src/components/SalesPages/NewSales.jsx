e12cc141a1bf0ff66f49e6863effd8abf253500a
Kavindu Lakshika <109098494+Kavindu-Lakshika@users.noreply.github.com>
2024-09-23 20:35:49 +0530

    Table

--- frontend/src/components/SalesPages/NewSales.jsx
+++ frontend/src/components/SalesPages/NewSales.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Table from '../Table/Table'
+
 
 const NewSales = () => {
+
+  const [data,] = useState([
+    ["Maleesha", "MaleeshaBalla@gmail.com"],
+  ]);
+
+  const columns = ["Name", "Email"];
+
+  const btnName = 'add'
+
   return (
     <div>
-        <h2>New Sale</h2>
-        
+      <h2>New Sale</h2>
+      <Table
+        data={data}
+        columns={columns}
+        btnName={btnName}
+      />
     </div>
   )
 }
