37b14cd196903043673dbdc0a1c7f42c63dd2d2d
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-09-27 14:44:09 +0530

    form responsive

--- frontend/src/Pages/StockReports/StockReports.jsx
+++ frontend/src/Pages/StockReports/StockReports.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
-import CurrentStock from '../../components/StockPages/CurrentStock'
 
 function StockReports() {
   return (
     <div>
         <Routes>
-        <Route path="current-stock" element={< CurrentStock/>} />
         
         
       </Routes>
