8c079e1d65867b34a099be332f422f0e4293df8a
Maleeshapa <maleeshapathirana1@gmail.com>
2024-09-28 23:12:21 +0530

    supp

--- frontend/src/Pages/Supplier/Supplier.jsx
+++ frontend/src/Pages/Supplier/Supplier.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SupplierDetails from './SupplierDetails'
 import SupplierPayments from './SupplierPayments'
+import NewStock from './NewStock'
 
 function Supplier() {
   return (
     <div>
         <Routes>
             <Route path='supplier' element={<SupplierDetails/>}/>
+            <Route path='new-stock' element={<NewStock/>}/>
             <Route path='supplier-payments' element={<SupplierPayments/>}/>
 
         </Routes>
--- frontend/src/Pages/Supplier/SupplierPayments.jsx
+++ frontend/src/Pages/Supplier/SupplierPayments.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Table from '../../components/Table/Table';
 
-const columns = ['#', 'Store Name', 'Supplier Name/Position','Date','images', 'Payment'];
+const columns = ['#', 'Store Name', 'Supplier Name/Position','Date','Total Amount', 'Paid' , 'Outstanding', 'Status'];
 
 const data = [
-  ['1', 'ABC Store', 'John Doe/Manager','2024-09-28 11.00AM','images', '1000'],
-  ['2', 'XYZ Store', 'Jane Smith/Supplier','2024-09-28 11.00AM','images', '1000']
+  ['1', 'ABC Store', 'John Doe/Manager','2024-09-28 11.00AM','5000', '4000', '1000', 'Status'],
+  ['2', 'XYZ Store', 'Jane Smith/Supplier','2024-09-28 11.00AM', '5000', '4000', '1000', 'Status']
 ];
 
 const btnName = ' + New Payment To Supplier ';
