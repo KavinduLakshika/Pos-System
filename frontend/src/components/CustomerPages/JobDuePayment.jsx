aef1cac7a392d8353c671d9d61cba91cacba5d3f
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-09-29 15:10:26 +0530

    create product

--- frontend/src/components/CustomerPages/JobDuePayment.jsx
+++ frontend/src/components/CustomerPages/JobDuePayment.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Table from '../Table/Table'
 
 const JobDuePayment = () => {
+
+  const [data,] = useState([
+    [],
+  ]);
+  const Columns = ["Job Id", 'Job Date', 'Customer', 'Invoice Amount','Due Amount','Creadit Age'];
+
   return (
     <div>
-      <h1>JobDuePayment</h1>
+      <div className="scrolling-container">
+        <h1>JobDuePayment</h1>
+        <Table
+            data={data}
+            columns={Columns}
+            showButton={false}
+          />
+      </div>
     </div>
   )
 }
