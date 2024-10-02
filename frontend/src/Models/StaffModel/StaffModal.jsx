ab3b363aea5736f9cb5ec8f5f5a7e44b2c5aa861
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-10-02 16:03:37 +0530

    staff model

--- frontend/src/Models/StaffModel/StaffModal.jsx
+++ frontend/src/Models/StaffModel/StaffModal.jsx
@@ -73,78 +73,80 @@ const StaffModal = ({ showModal, closeModal, handleSubmit, editData }) => {
 
 
   return (
-    <div className="scrolling-container">
-      <div className="modal-overlay">
-        <div className="modal-content">
-          <h3 className='mb-3'>{editData ? 'Edit Staff Member' : 'Add New Staff Member'}</h3>
-          <form onSubmit={handleFormSubmit}>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h3 className='mb-3'>{editData ? 'Edit Staff Member' : 'Add New Staff Member'}</h3>
+        <form onSubmit={handleFormSubmit}>
 
-            <div className="form-group">
-              <div className="photo-container">
-                <img
-                  src={formData.photo || 'https://via.placeholder.com/150'}
-                  alt="User"
-                  className="user-photo"
+          <div className="form-group">
+            <div className="photo-container">
+              <img
+                src={formData.photo || 'https://via.placeholder.com/150'}
+                alt="User"
+                className="user-photo"
+              />
+              <div className="edit-icon">
+                <label htmlFor="photo-input">
+                  <FontAwesomeIcon icon={faEdit} />
+                </label>
+                <input
+                  id="photo-input"
+                  type="file"
+                  accept="image/*"
+                  style={{ display: 'none' }}
+                  onChange={handlePhotoChange}
                 />
-                <div className="edit-icon">
-                  <label htmlFor="photo-input">
-                    <FontAwesomeIcon icon={faEdit} />
-                  </label>
-                  <input
-                    id="photo-input"
-                    type="file"
-                    accept="image/*"
-                    style={{ display: 'none' }}
-                    onChange={handlePhotoChange}
-                  />
-                </div>
+              </div>
+            </div>
+          </div>
+          <div className="form-flex">
+            <div className="form-1">
+              <div className="form-group">
+                <label>Department / Job Position</label>
+                <input type="text" name="department" value={formData.department} onChange={handleChange} required />
+              </div>
+              <div className="form-group">
+                <label>Full Name</label>
+                <input type="text" name="fullName" value={formData.fullName} onChange={handleChange} required />
+              </div>
+              <div className="form-group">
+                <label>User Name <span>*</span></label>
+                <input type="text" name="userName" value={formData.userName} onChange={handleChange} placeholder="Enter User Name" required />
+              </div>
+              <div className="form-group">
+                <label>Email <span>*</span></label>
+                <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Enter Email" required />
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Enter Password" />
               </div>
             </div>
 
-            <div className="form-group">
-              <label>Department / Job Position</label>
-              <input type="text" name="department" value={formData.department} onChange={handleChange} required />
+            <div className="form-1">
+              <div className="form-group">
+                <label>Contact 1</label>
+                <input type="text" name="contact1" value={formData.contact1} onChange={handleChange} required />
+              </div>
+              <div className="form-group">
+                <label>Contact 2</label>
+                <input type="text" name="contact2" value={formData.contact2} onChange={handleChange} />
+              </div>
+              <div className="form-group">
+                <label>Address</label>
+                <input type="text" name="address" value={formData.address} onChange={handleChange} required />
+              </div>
+              <div className="form-group">
+                <label>NIC</label>
+                <input type="text" name="nic" value={formData.nic} onChange={handleChange} required />
+              </div>
+              <div className=" form-group mt-4">
+              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="button" className="btn btn-danger" onClick={closeModal}>Close</button>
+              </div>
             </div>
-            <div className="form-group">
-              <label>Full Name</label>
-              <input type="text" name="fullName" value={formData.fullName} onChange={handleChange} required />
-            </div>
-            <div className="form-group">
-              <label>User Name <span>*</span></label>
-              <input type="text" name="userName" value={formData.userName} onChange={handleChange} placeholder="Enter User Name" required />
-            </div>
-            <div className="form-group">
-              <label>Email <span>*</span></label>
-              <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Enter Email" required />
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Enter Password" />
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Enter Password" />
-            </div>
-            <div className="form-group">
-              <label>Contact 1</label>
-              <input type="text" name="contact1" value={formData.contact1} onChange={handleChange} required />
-            </div>
-            <div className="form-group">
-              <label>Contact 2</label>
-              <input type="text" name="contact2" value={formData.contact2} onChange={handleChange} />
-            </div>
-            <div className="form-group">
-              <label>Address</label>
-              <input type="text" name="address" value={formData.address} onChange={handleChange} required />
-            </div>
-            <div className="form-group">
-              <label>NIC</label>
-              <input type="text" name="nic" value={formData.nic} onChange={handleChange} required />
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-            <button type="button" className="btn btn-secondary" onClick={closeModal}>Close</button>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   );
