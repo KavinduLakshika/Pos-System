31a4fc9e7a1a480f2cd51c58a315076a1b4d55ba
Maleeshapa <maleeshapathirana1@gmail.com>
2024-10-03 15:19:30 +0530

    dashboard

--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Dashboard.css'
+import CardOne from './CardOne';
+import CardTwo from './CardTwo';
+
 
 const Dashboard = () => {
+
+  const TodayTotal = 'Rs. 10,000';
+  const YesterdayTotal = 'Rs. 10,000';
+  const ThisMonthTotal = 'Rs. 10,000';
+  const LastMonthTotal = 'Rs. 10,000';
+
+  const  JanTotal = 65;
+  const  FebTotal =59;
+  const  MarTotal = 89;
+  const  AprTotal = 81;
+  const  JunTotal = 55;
+
   return (
     <div className="d-flex">
-      <main style={{marginLeft: '30px', width: 'calc(100% - 60px)', padding: '20px'}}>
+      <main style={{ padding: '20px' }}>
         <h1 className="h2 mb-4">Dashboard</h1>
-        <p>Welcome to your POS System Dashboard. Select an option from the menu to get started.</p>
+
+        <div className='d-flex'>
+
+          <div className="me-3"> 
+            <CardOne 
+            TodayTotal={TodayTotal}
+            YesterdayTotal={YesterdayTotal}
+            ThisMonthTotal={ThisMonthTotal}
+            LastMonthTotal={LastMonthTotal}/>
+
+          </div>
+
+          <div className="me-3"> 
+            <CardTwo 
+            JanTotal={JanTotal}
+            FebTotal={FebTotal}
+            MarTotal={MarTotal}
+            AprTotal={AprTotal}
+            JunTotal={JunTotal}
+            />
+            
+          </div>
+
+        </div>
+
+
+
+
+
+
       </main>
+
     </div>
   );
 };
