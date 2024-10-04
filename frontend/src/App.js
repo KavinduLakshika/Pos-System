9c4c3e09955589d0b05a31d4bc1b298dbbe74575
Maleeshapa <maleeshapathirana1@gmail.com>
2024-10-04 18:46:55 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SideBar from './components/SideBar/SideBar';
 import Dashboard from './Pages/Dashboard/Dashboard';
@@ -13,29 +13,41 @@ import StockReports from './Pages/StockReports/StockReports';
 import SalesReports from './Pages/SalesReport/SalesReports';
 import Supplier from './Pages/Supplier/Supplier';
 import Rental from './Pages/Rental/Rental';
+import Login from './Pages/Login';
+
+function Layout() {
+  const location = useLocation();
+  
+  // Check if the current path is "/login" (case insensitive)
+  const isLoginPage = location.pathname.toLowerCase() === '/login';
+
+  return (
+    <div className="d-flex">
+      {!isLoginPage && <SideBar />}
+      <div style={{ flexGrow: 1, margin: '20px 0px' }}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/sales/*" element={<Sales />} />
+          <Route path="/rental/*" element={<Rental />} />
+          <Route path="/customer/*" element={<Customer />} />
+          <Route path="/product/*" element={<Product />} />
+          <Route path="/grn/*" element={<GRN />} />
+          <Route path="/stock/*" element={<Stock />} />
+          <Route path="/supplier/*" element={<Supplier />} />
+          <Route path="/sales-reports/*" element={<SalesReports />} />
+          <Route path="/stock-reports/*" element={<StockReports />} />
+          <Route path="/staff/*" element={<Staff />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="d-flex" >
-      <SideBar />
-      <div style={{ flexGrow: 1, margin: '20px 0px' }}>
-        <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/sales/*" element={<Sales />} />
-        <Route path="/rental/*" element={<Rental />} />
-        <Route path="/customer/*" element={<Customer />} />
-        <Route path="/product/*" element={<Product />} />
-        <Route path="/grn/*" element={<GRN />} />
-        <Route path="/stock/*" element={<Stock />} />
-        <Route path="/supplier/*" element={<Supplier />} />
-        <Route path="/sales-reports/*" element={<SalesReports />} />
-        <Route path="/stock-reports/*" element={<StockReports />} />
-        <Route path="/staff/*" element={<Staff />} />
-
-        </Routes>
-        </div>
-      </div>
+      <Layout />
     </BrowserRouter>
   );
 }
